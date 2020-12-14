086c47e50c50a9b10c77eb2cd8498b1b588db391
Thiago <thiago.kamada@gmail.com>
2020-12-14 16:23:48 +0900

    Including view

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,20 +1,24 @@
 class Negociacao {
-
+    
     constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
 
     get data() {
-        return this._data
+
+        return this._data;
     }
 
     get quantidade() {
-        return this._quantidade
+
+        return this._quantidade;
     }
 
     get valor() {
-        return this._valor
+
+        return this._valor;
     }
 
     get volume() {
+
         return this._quantidade * this._valor;
     }
 }
